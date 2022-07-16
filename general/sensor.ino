0f3ce93c6a0f36d4cfa1928054c64f73c8b75768
ArtyTBcat <77570219+ArtyTBcat@users.noreply.github.com>
2022-07-16 13:10:11 +0700

    sensor code

--- general/sensor.ino
+++ general/sensor.ino
@@ -1,7 +1,25 @@
+/*
+MIT License
+Copyright (c) 2022 ArtyTBcat
+Author: Nontakorn Bunluesriruang
+*/
+
+//define Ultrasonic pins
+#define trig_r 3
+#define echo_r 4
+#define trig_c 5
+#define echo_c 6
+#define trig_l 7
+#define echo_l 8
+
 void setup(){
+    int inputPins[3] = {echo_r, echo_c, echo_l};
+    int outputPins[3] = {trig_r, trig_c, trig_l};
+    for {x,}
 
 }
 
 void loop(){
 
 }
+
