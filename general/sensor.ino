1c14923ef8caa1678a004741116bfc5b27cf4a45
ArtyTBcat <77570219+ArtyTBcat@users.noreply.github.com>
2022-07-16 20:16:48 +0700

    init

--- general/sensor.ino
+++ general/sensor.ino
@@ -4,19 +4,28 @@ Copyright (c) 2022 ArtyTBcat
 Author: Nontakorn Bunluesriruang
 */
 
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
 //define Ultrasonic pins
-#define trig_r 3
-#define echo_r 4
-#define trig_c 5
-#define echo_c 6
-#define trig_l 7
-#define echo_l 8
+#define gpsRX 3
+#define gpsTX 4
+#define trig_r 5
+#define echo_r 6
+#define trig_c 7
+#define echo_c 8
+#define trig_l 9
+#define echo_l 10
 
 int slave_address = 0x53;
 int inputPins[3] = {echo_r, echo_c, echo_l};
 int outputPins[3] = {trig_r, trig_c, trig_l};
 
-void setup(){    
+TinyGPSPlus gps;
+SoftwareSerial ss(gpsRX, gpsTX);
+
+void setup(){
+    ss.begin(GPSBaud);
     for (int i = 0; i <= 3; i++) {
         pinMode(inputPins[i], INPUT);
         pinMode(outputPins[i], OUTPUT);
@@ -25,6 +34,26 @@ void setup(){
 
 void loop()
 {
+    while (ss.available() > 0) {
+        gps.encode(ss.read());
+        if (gps.location.isUpdated()) {
+            // Latitude in degrees (double)
+            Serial.print("Latitude= ");
+            Serial.print(gps.location.lat(), 6);
+            // Longitude in degrees (double)
+            Serial.print(" Longitude= ");
+            Serial.println(gps.location.lng(), 6);
+            // speed km/hr
+            Serial.print("Speed in km/h = ");
+            Serial.println(gps.speed.kmph());
+            // Course in degrees (double)
+            Serial.print("Course in degrees = ");
+            Serial.println(gps.course.deg());
+            // Altitude in meters (double)
+            Serial.print("Altitude in meters = ");
+            Serial.println(gps.altitude.meters());
+        }
+    }
 }
 
 float range(){
